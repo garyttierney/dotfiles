d92e5fca3e9c0d9816911937be8235e40d432bd2
Gary Tierney <gary.tierney@fastmail.com>
2022-11-21 19:31:27 +0000

    Use Vulkan renderer for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,16 @@
 return {
-  color_scheme = "GitHub Dark",
+  color_scheme = "tokyonight",
   audible_bell = "Disabled",
   max_fps = 144,
   window_decorations = "RESIZE",
+  enable_wayland = false, -- https=//github.com/wez/wezterm/issues/1701
+  front_end = "WebGpu",
+  webgpu_preferred_adapter = {
+    backend = "Vulkan",
+    device_type = "DiscreteGpu",
+    name = "NVIDIA GeForce GTX 1660 Ti",
+  },
+  keys = {
+    { key = 'f', mods = 'CTRL | SHIFT', action = 'DisableDefaultAssignment' }
+  }
 }
