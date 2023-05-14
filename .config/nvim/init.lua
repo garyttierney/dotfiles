04a6c4471f9641b2519692f914ba3c4f1ef4ee49
Gary Tierney <gary.tierney@fastmail.com>
2023-05-14 20:47:19 +0100

    Show highlights/diagnostics in a scroll bar with satellite

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -64,8 +64,37 @@ require('lazy').setup({
   {
     'lvimuser/lsp-inlayhints.nvim',
     branch = "anticonceal",
-    opts = {},
   },
+  {
+    'lewis6991/satellite.nvim',
+    config = function()
+      require('satellite').setup({
+        current_only = false,
+        winblend = 50,
+        zindex = 40,
+        excluded_filetypes = {},
+        width = 2,
+        handlers = {
+          search = {
+            enable = true,
+          },
+          diagnostic = {
+            enable = true,
+            signs = { '-', '=', '≡' },
+            min_severity = vim.diagnostic.severity.HINT,
+          },
+          gitsigns = {
+            enable = false,
+          },
+          marks = {
+            enable = true,
+            show_builtins = false, -- shows the builtin marks like [ ] < >
+          },
+        },
+      })
+    end
+  },
+  -- Breadcrumbs at the top of windows
   {
     "utilyre/barbecue.nvim",
     name = "barbecue",
